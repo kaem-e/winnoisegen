09fc0d5be3a0817255e5b8b877ff0a75756182f7
Kaem <46230985+kaem-e@users.noreply.github.com>
2026-01-26 15:04:00 +0500

    do deinterleaving for input & show how to do deinterleaving in stream callback

--- src/main.rs
+++ src/main.rs
@@ -2,27 +2,36 @@
 #![feature(float_minimum_maximum)]
 
 use cpal::{
-	self,
+	self, Host,
 	traits::{DeviceTrait, HostTrait, StreamTrait},
 };
 use rand;
 
 fn main() -> Result<(), anyhow::Error> {
-	let device = cpal::default_host()
+	let device = Host::default()
 		.default_output_device()
 		.ok_or_else(|| anyhow::Error::msg("Failed to get default output device"))?;
+
 	let config = device
 		.supported_output_configs()?
 		.next()
 		.ok_or_else(|| anyhow::Error::msg("No supported stream config"))?
-		.with_max_sample_rate();
+		.with_max_sample_rate()
+		.config();
+
 
 	let stream = device.build_output_stream(
-		&config.config(),
-		|a: &mut [f32], _| {
-			for i in a {
-				*i = rand::random::<f32>();
+		&config,
+		move |b: &mut [f32], _| {
+			// Deinterleave mono
+			for i in b.chunks_mut(config.channels as usize) {
+				let s = rand::random::<f32>();
+				i.fill(s);
 			}
+
+			// for i in a {
+			// 	*i = rand::random::<f32>();
+			// }
 		},
 		|_| {},
 		None,
