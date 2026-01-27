8f1d5841e7d604eebdfefcb9ffb21b3792c62c5d
Kaem <46230985+kaem-e@users.noreply.github.com>
2026-01-27 20:51:22 +0500

    Make cpal work

--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,121 @@
 #![cfg_attr(all(windows, not(debug_assertions)), windows_subsystem = "windows")]
-#![feature(float_minimum_maximum, portable_simd)]
-#![allow(unused)]
+#![feature(float_minimum_maximum, portable_simd, if_let_guard)]
 
-// use cpal::{
-// 	self, Host,
-// 	traits::{DeviceTrait, HostTrait, StreamTrait},
-// };
-// use egui_winit;
-// use rand;
-// use egui;
+use cpal::{
+	self, Host, Stream,
+	traits::{DeviceTrait, HostTrait, StreamTrait},
+};
 use winit::{
 	self,
 	application::ApplicationHandler,
-	event::WindowEvent,
+	dpi::LogicalSize,
+	event::{ElementState, KeyEvent, WindowEvent},
 	event_loop::{ActiveEventLoop, ControlFlow, EventLoop},
+	keyboard::{KeyCode, PhysicalKey},
 	window::{Window, WindowId},
 };
 
-// fn main() -> Result<(), anyhow::Error> {
-// 	let device = Host::default()
-// 		.default_output_device()
-// 		.ok_or_else(|| anyhow::Error::msg("Failed to get default output device"))?;
-
-// 	let config = device
-// 		.supported_output_configs()?
-// 		.next()
-// 		.ok_or_else(|| anyhow::Error::msg("No supported stream config"))?
-// 		.with_max_sample_rate()
-// 		.config();
-
-// 	let stream = device.build_output_stream(
-// 		&config,
-// 		move |b: &mut [f32], _| {
-// 			// Deinterleave mono
-// 			for i in b.chunks_mut(config.channels as usize) {
-// 				let s = rand::random::<f32>();
-// 				i.fill(s);
-// 			}
-
-// 			// for i in a {
-// 			// 	*i = rand::random::<f32>();
-// 			// }
-// 		},
-// 		|_| {},
-// 		None,
-// 	)?;
-
-// 	stream.play()?;
-
-// 	std::thread::sleep(std::time::Duration::from_millis(2000));
-
-// 	Ok(())
-// }
-
 fn main() -> Result<(), anyhow::Error> {
+	let device = Host::default()
+		.default_output_device()
+		.ok_or_else(|| anyhow::Error::msg("Failed to get default output device"))?;
+
+	let config = device
+		.supported_output_configs()?
+		.find_map(|a| {
+			if a.max_sample_rate() == 48000 && cpal::SampleFormat::F32 == a.sample_format() {
+				Some(a.with_max_sample_rate().config())
+			} else {
+				None
+			}
+		})
+		.ok_or_else(|| anyhow::Error::msg("No supported stream config"))?;
+
+	let stream = device.build_output_stream(
+		&config,
+		move |b: &mut [f32], _| {
+			// // Deinterleave mono
+			// for i in b.chunks_mut(config.channels as usize) {
+			// 	i.fill(rand::random::<f32>());
+			// }
+
+			for i in b {
+				*i = rand::random::<f32>();
+			}
+		},
+		|_| {},
+		None,
+	)?;
+
 	let event_loop = EventLoop::new()?;
 	event_loop.set_control_flow(ControlFlow::Wait);
 
-	let mut app = App::default();
-	event_loop.run_app(&mut app);
+	let mut app = App::new(stream);
+	event_loop.run_app(&mut app)?;
 
 	Ok(())
 }
 
-#[derive(Default)]
 struct App {
-	egui_ctx: egui::Context,
+	// egui_ctx: egui::Context,
+	cpal: (Stream, bool),
 	window: Option<Window>,
+}
+
+impl App {
+	pub fn new(cpal_stream: Stream) -> Self {
+		Self {
+			cpal: (cpal_stream, false),
+			window: None,
+		}
+	}
 }
 
 impl ApplicationHandler for App {
 	fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-		self.window = Some(
-			event_loop
-				.create_window(Window::default_attributes())
-				.unwrap(),
-		);
+		self.window = event_loop
+			.create_window(
+				Window::default_attributes()
+					.with_min_inner_size(LogicalSize::new(400.0, 200.0))
+					.with_max_inner_size(LogicalSize::new(800.0, 500.0))
+					.with_title("Penis Balls Sex"),
+			)
+			.ok();
 	}
 
 	fn window_event(&mut self, event_loop: &ActiveEventLoop, _: WindowId, event: WindowEvent) {
 		match event {
-			WindowEvent::CloseRequested => {
-				println!("The close button was pressed; stopping");
+			// close window with close event or by pressing escape
+			WindowEvent::CloseRequested
+			| WindowEvent::KeyboardInput {
+				event:
+					KeyEvent {
+						physical_key: PhysicalKey::Code(KeyCode::Escape),
+						state: ElementState::Pressed,
+						..
+					},
+				..
+			} => {
 				event_loop.exit();
 			},
+
+			// spacebar to toggle play/pause
+			WindowEvent::KeyboardInput {
+				event:
+					KeyEvent {
+						physical_key: PhysicalKey::Code(KeyCode::Space),
+						state: ElementState::Pressed,
+						..
+					},
+				..
+			} => {
+				match dbg!(self.cpal.1) {
+					true => self.cpal.0.pause().unwrap(),
+					false => self.cpal.0.play().unwrap(),
+				};
+				self.cpal.1 = !self.cpal.1;
+			},
+
 			WindowEvent::RedrawRequested => {
 				// Redraw the application.
 				//
@@ -100,9 +132,7 @@ impl ApplicationHandler for App {
 				// can render here instead.
 				self.window.as_ref().unwrap().request_redraw();
 			},
-			a => {
-				dbg!(a);
-			},
+			_ => {},
 		}
 	}
 }
